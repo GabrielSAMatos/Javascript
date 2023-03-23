b93ec808b6f624031ee535cc415023151a539149
Gabriel <114437365+GabrielSAMatos@users.noreply.github.com>
2023-03-22 22:23:21 -0300

    html e css feitos.

---
+++ exercicios/Projetos/Calculadora_Basica/script.js
@@ -0,0 +1,12 @@
+const num0 = document.querySelector("zero")
+const num1 = document.querySelector("um")
+const num2 = document.querySelector("dois")
+const num3 = document.querySelector("tres")
+const num4 = document.querySelector("quatro")
+const num5 = document.querySelector("cinco")
+const num6 = document.querySelector("seis")
+const num7 = document.querySelector("sete")
+const num8 = document.querySelector("oito")
+const num9 = document.querySelector("nove")
+
+const tela = 
